c00813ed3e53bd59d3ea93cbf602c5d3e91a2538
Ruthvik <dacha.ruthvik@gmail.com>
2026-05-23 19:46:52 -0400

    Import timedelta from datetime in model.py

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ import pandas as pd
 import pickle
 import json
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from xgboost import XGBRegressor
